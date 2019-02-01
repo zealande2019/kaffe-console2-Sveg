6f55bfac9fb6d941bb4713f40b8d072332bb49a0
Sveg <36763190+Sveg@users.noreply.github.com>
2019-02-01 13:54:26 +0100

    Added FlatWhite og liste af obj

---
+++ KaffeConsoleAppNetCore/FlatWhite.cs
@@ -0,0 +1,28 @@
+﻿using kaffe;
+
+namespace KaffeConsoleAppNetCore
+{
+    public class FlatWhite : Kaffe, Imælk
+    {
+        public FlatWhite(int rabat): base(rabat)
+        {
+              
+        }
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public override string Styrke()
+        {
+            return "mild";
+        }
+
+  
+    }
+}
--- KaffeConsoleAppNetCore/Program.cs
+++ KaffeConsoleAppNetCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using kaffe;
 using System;
+using System.Collections.Generic;
 
 namespace KaffeConsoleAppNetCore
 {
@@ -8,10 +9,30 @@ namespace KaffeConsoleAppNetCore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
             
+            Cortado cortado = new Cortado();
+            Latte latte = new Latte();
+            FlatWhite flatWhite = new FlatWhite(14);
 
-           
+            List<Imælk> liste = new List<Imælk>();
+            liste.Add(latte);
+            liste.Add(cortado);
+            liste.Add(flatWhite);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine("Før cast" + item.MlMælk());
+                if (item is Latte)
+                {
+                    Latte l = item as Latte;
+                    Console.WriteLine("Latte styrke" + l.Styrke());
+                }
+                else if (item is Cortado)
+                {
+                    Cortado c = item as Cortado;
+                }
+                
+            }
 
         }
     }
